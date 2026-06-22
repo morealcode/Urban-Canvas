af72c319ddbbf7b0fac6616f5bd077486d8da690
morealcode <235274488+morealcode@users.noreply.github.com>
2026-06-22 10:31:07 +0200

    Initial Commit

---
+++ Urban Canvas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Urban Canvas
+//
+//  Created by apprenant130 on 22/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Urban Canvas/Urban_CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Urban_CanvasApp.swift
+//  Urban Canvas
+//
+//  Created by apprenant130 on 22/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Urban_CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
