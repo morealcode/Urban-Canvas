d111585581c84c465a3d035e621866eb13093542
morealcode <235274488+morealcode@users.noreply.github.com>
2026-06-23 10:37:13 +0200

    Add filter view and logic

---
+++ Urban Canvas/Components/ArtworkFilterView.swift
@@ -0,0 +1,93 @@
+//
+//  ArtworkFilterView.swift
+//  Urban Canvas
+//
+//  Created by morealcode on 23/06/2026.
+//
+
+import SwiftUI
+
+struct ArtworkFilterView: View {
+
+    @Binding var isShowing: Bool
+    @Binding var selectedStyle: ArtworkStyle?
+
+    var body: some View {
+
+        VStack(alignment: .leading, spacing: 10) {
+
+            Text("Liste des filtres")
+                .fontWeight(.semibold)
+
+            Text("Choisissez un type d'art")
+                .padding(.bottom, 14)
+
+            FilterButton(
+                title: "Tous",
+                isSelected: selectedStyle == nil
+            ) {
+                withAnimation {
+                    selectedStyle = nil
+                }
+            }
+
+            ForEach(ArtworkStyle.allCases, id: \.self) { style in
+
+                FilterButton(
+                    title: style.rawValue,
+                    isSelected: selectedStyle == style
+                ) {
+                    withAnimation {
+                        selectedStyle = style
+                    }
+                }
+            }
+
+        }
+        .frame(width: 300)
+        .padding(24)
+    }
+}
+
+struct FilterButton: View {
+    let title: String
+    let isSelected: Bool
+    let action: () -> Void
+
+    var body: some View {
+        if isSelected {
+            Button(
+                action: action,
+                label: {
+                    Text(title)
+                        .fontWeight(.medium)
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 13)
+                }
+            )
+            .buttonStyle(.bordered)
+            .foregroundStyle(Color.accentColor)
+            .buttonSizing(.flexible)
+        } else {
+            Button(
+                action: action,
+                label: {
+                    Text(title)
+                        .fontWeight(.medium)
+                        .padding(.horizontal, 16)
+                        .padding(.vertical, 13)
+                }
+            )
+            .buttonStyle(.bordered)
+            .foregroundStyle(.mainText)
+            .buttonSizing(.flexible)
+        }
+    }
+}
+
+#Preview {
+    @Previewable @State var isShowing: Bool = true
+    @Previewable @State var selectedStyle: ArtworkStyle? = nil
+
+    ArtworkFilterView(isShowing: $isShowing, selectedStyle: $selectedStyle)
+}
--- Urban Canvas/ContentView.swift
+++ Urban Canvas/ContentView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var isShowingFilter: Bool = false
+    @State private var selectedStyle: ArtworkStyle? = nil
+
     @State private var artworkViewModel: ArtworkViewModel = ArtworkViewModel()
+
+    private var filteredArtworks: [Artwork] {
+
+        guard let isStyleSelected = selectedStyle else {
+            return artworkViewModel.artworks
+        }
+
+        return artworkViewModel.artworks.filter { $0.type == selectedStyle }
+    }
 
     var body: some View {
         NavigationStack {
-            ArtworkListView(artworks: artworkViewModel.artworks)
+            ArtworkListView(artworks: filteredArtworks)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button(
+                            "Filtre",
+                            systemImage: "line.3.horizontal.decrease.circle"
+                        ) {
+                            isShowingFilter = true
+                        }
+                        .popover(isPresented: $isShowingFilter) {
+                            ArtworkFilterView(
+                                isShowing: $isShowingFilter,
+                                selectedStyle: $selectedStyle
+                            )
+                            .presentationCompactAdaptation(.none)
+                        }
+                    }
+                }
         }
     }
 }
